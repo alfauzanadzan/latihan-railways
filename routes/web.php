a0de92a815ef163fd0cb548c4e5461b76741f33e
alfauzanadzan <alfauzanadzan81@gmail.com>
2025-05-28 10:57:01 +0700

    nambah views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PenggunaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/beranda', [PenggunaController::class, 'index']);
